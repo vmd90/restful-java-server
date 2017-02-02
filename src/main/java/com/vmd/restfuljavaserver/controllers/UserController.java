9cc3d660a7f7e63e00eddb91187b519de54ee741
Victor <victor.munidario@gmail.com>
2017-02-02 19:11:57 -0200

    .

--- src/main/java/com/vmd/restfuljavaserver/controllers/UserController.java
+++ src/main/java/com/vmd/restfuljavaserver/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import javax.ws.rs.DefaultValue;
 
 import com.vmd.restfuljavaserver.models.User;
 import com.vmd.restfuljavaserver.repos.UserRepository;
